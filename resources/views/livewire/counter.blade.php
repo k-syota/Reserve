bbb2fbe6a8f3ec315aa0c4fdc5754b7f83777e35
Syota Nishimura <syota4723@gmail.com>
2022-10-05 22:09:08 +0900

    :adhesive_bandage: データバインディングのオプションについて

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,10 @@
 <div style="text-align: center">
     <button wire:click="increment">+</button>
     <h1>{{ $count }}</h1>
+    こんにちは {{ $name }}さん<br>
+    {{-- ms待ってから通信 --}}
+    {{-- <input type="text" wire:model.debounce.2000ms="name"> --}}
+    {{-- フォーカスが外れたタイミングで --}}
+    <input type="text" wire:model.lazy="name">
 
-    <input type="text" wire:model="name">
-    こんにちは {{ $name }}さん
 </div>
