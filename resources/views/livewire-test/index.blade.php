419b06754b434e8304675007ae06606206e91b35
Syota Nishimura <syota4723@gmail.com>
2022-10-05 21:58:01 +0900

    :sparkles: livewireを試してみる

---
+++ resources/views/livewire-test/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @livewireStyles
+</head>
+<body>
+    Livewire-Test
+    {{-- <livewire:counter /> --}}
+    @livewire("counter")
+    
+    @livewireScripts
+</body>
+</html>
