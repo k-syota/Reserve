9e05e861f58a98c6ec866241fc4db50e7500c19b
Syota Nishimura <syota4723@gmail.com>
2022-11-14 22:42:07 +0900

    :seedling: ReservationSeederを更新

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -29,6 +29,18 @@ class ReservationSeeder extends Seeder
                 "canceled_date" => null
             ],
             [
+                "user_id" => "1",
+                "event_id" => "3",
+                "number_of_people" => "3",
+                "canceled_date" => "2022-11-1 09:00:00"
+            ],
+            [
+                "user_id" => "2",
+                "event_id" => "1",
+                "number_of_people" => "10",
+                "canceled_date" => "2022-11-1 09:00:00"
+            ],
+            [
                 "user_id" => "2",
                 "event_id" => "10",
                 "number_of_people" => "11",
