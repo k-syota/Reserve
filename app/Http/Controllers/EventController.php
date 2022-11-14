814cf8971fa8c3b309a658c26f5b517915945db6
Syota Nishimura <syota4723@gmail.com>
2022-11-14 22:43:14 +0900

    :adhesive_bandage: events@indexでキャンセル分を人数に含まないように修正

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -23,6 +23,7 @@ class EventController extends Controller
 
         $reservedPeople = DB::table("reservations")
         ->select("event_id",DB::raw('sum(number_of_people) as number_of_people'))
+        ->whereNull("canceled_date")
         ->groupBy("event_id");
 
         // dd($reservedPeople);
@@ -175,6 +176,7 @@ class EventController extends Controller
 
         $reservedPeople = DB::table("reservations")
         ->select("event_id",DB::raw('sum(number_of_people) as number_of_people'))
+        ->whereNull("canceled_date")
         ->groupBy("event_id");
 
         $events = DB::table("events")
