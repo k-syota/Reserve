38ae5c3c19a300836d1ea2539e71329fa3d7c292
Syota Nishimura <syota4723@gmail.com>
2022-11-15 21:29:36 +0900

    :sparkles: livewireで予約カレンダーを作成

---
+++ resources/views/livewire/calendar.blade.php
@@ -0,0 +1,11 @@
+<div>
+    カレンダー
+    <x-jet-input id="calendar" class="block mt-1 w-full" type="text" name="calendar"
+    :value="old('event_date')"/>
+    {{ $currentDate }}
+    <div class="flex">
+        @for ($day = 0; $day < 7; $day++)
+            {{ $currentWeek[$day] }}        
+        @endfor
+    </div>
+</div>
