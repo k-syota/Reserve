cc4f43984ffa8f452cb95f652c98373ba47ba104
Syota Nishimura <syota4723@gmail.com>
2022-11-16 22:04:04 +0900

    :seedling: ダミーデータを修正

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -18,13 +18,21 @@ class EventFactory extends Factory
     {
 
         $dummyDate = $this->faker->dateTimeThisMonth;
+        $valiableHour = $this->faker->numberBetween(10,18);
+        $minutes = [0,30];
+        $mKey = array_rand($minutes);
+        $addHour = $this->faker->numberBetween(1,3);
+
+        $startDate = $dummyDate->setTime($valiableHour,$minutes[$mKey]);
+        $clone = clone $startDate;
+        $endDate = $clone->modify('+'.$addHour.'hour');
 
         return [
             'name' => $this->faker->name,
             'information' => $this->faker->realText,
             'max_people' => $this->faker->numberBetween(1,20),
-            'start_date' => $dummyDate->format('Y-m-d H:i:s'),
-            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
             'is_visible' => $this->faker->boolean,
         ];
     }
