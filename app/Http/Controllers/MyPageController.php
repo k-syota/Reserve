b73d255e65a4b3f6697bd325096a5f9ce4ec67f2
Syota Nishimura <syota4723@gmail.com>
2022-11-23 15:34:36 +0900

    :sparkles: マイページshowとキャンセル処理を実装

--- app/Http/Controllers/MyPageController.php
+++ app/Http/Controllers/MyPageController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Reservation;
 use App\Models\User;
 use App\Services\MyPageService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,5 +25,29 @@ class MyPageController extends Controller
         // dd($fromPastEvents,$fromTodayEvents);
 
         return view('mypage/index',compact('fromTodayEvents','pastEvents'));
+    }
+
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+        $reservation = Reservation::where('user_id', '=', Auth::id())
+        ->where('event_id', '=', $id)
+        ->first();
+
+        return view('mypage/show',compact("event",'reservation'));
+    }
+
+    public function cancel($id)
+    {
+        $event = Event::findOrFail($id);
+        $reservation = Reservation::where('user_id', '=', Auth::id())
+        ->where('event_id', '=', $id)
+        ->first();
+
+        $reservation->canceled_date = Carbon::now()->format('Y-m-d H:i:s');
+        $reservation->save();
+
+        session()->flash('status','キャンセルしました');
+        return to_route('dashboard');
     }
 }
